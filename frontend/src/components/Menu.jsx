afcf703216889d2807a42f1f312c85f15b202707
Naresh123se <naresh132na@gmail.com>
2024-12-20 23:22:38 +0545

    update feather

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Menu = () => {
+  const mobile = (mobile) => {
+    return <h1>mm</h1>;
+  };
+
+  const Desktop = (Desktop) => {
+    return <h1>ll</h1>;
+  };
+
+  return (
+    <div>
+      <></>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="h-20">
+        {/* mobile */}
+        <div className="h-full flex items-center justify-between md:gap-8 ">
+          <div className="">logo</div>
+          <div ={mobile} >
+            okok
+          </div>
+
+          <div ={Desktop} >
+            pp
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
